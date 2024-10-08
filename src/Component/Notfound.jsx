9d2f9953a7d00dd0d1920a6b3d1f171c7d64e628
AhmedAbdelsalam 11 <ahmdabslam111@gmail.com>
2024-10-08 15:37:38 +0300

    second commit

--- src/Component/Notfound.jsx
+++ src/Component/Notfound.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Notfound = () => {
     return (
-        <div>
+        <div className='vh-100 d-flex align-items-center justify-content-center'>
             <h1 className='pt-5'>page not found</h1>
             <p>This page doesn’t exist.</p>
             <p>If this is a mistake, let us know, and we will try to fix it!</p>
